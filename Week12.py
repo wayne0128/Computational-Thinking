e8dcd6f27498d7a3dc6198821369858149662395
wayne0128 <71250610+wayne0128@users.noreply.github.com>
2020-12-06 20:44:16 +0800

    Add files via upload

---
+++ Week12.py
@@ -0,0 +1,33 @@
+hello1  =  “你好”
+hello2  =  “世界”
+hello3  =  “ Shaquile O'Neil”
+
+s1  =  “ 123”
+s2  =  “ 456”
+s3  =  s1  +  s2
+打印（s3）
+s4  =  s1  *  2
+打印（s4）
+
+s1  =  “每天一個蘋果。”
+s1。大寫（）
+s1。標題（）
+s1。互換（）
+s1。上（）
+s1。降低（）
+numberlist  = [ 1，2，3，4，5 ]
+charlist  = [ “ 1”，“ 2”，“ 3”，“ 4”，“ 5” ]
+mixlist = [ 1，“ 2”，3，“ 4”，5 ]
+
+字 =  “ Python”
+list1  = 列表（單詞）
+列表2  = [ “顏色”，“動物”，1，2，3 ]
+季節 =  “ 1995-1996”
+team  =  “芝加哥公牛”
+coach  =  “ Phil Jackson”
+記錄 = [ 72，10 ]
+starting_lineup  = [ “羅恩·哈珀”，“邁克爾·喬丹”，“斯科蒂·皮蓬”，“丹尼斯·羅德曼”，“盧克·朗利” ]
+won_championship  = 真
+best_chicago_bulls  = 列表（（賽季，球隊，教練，記錄，starting_lineup，won_championship））
+打印（best_chicago_bulls）
+打印（類型（best_chicago_bulls））
